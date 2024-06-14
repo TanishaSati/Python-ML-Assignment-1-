64cfac4aa0e09c5d3858fb7670c4410bab488523
TanishaSati <142687396+TanishaSati@users.noreply.github.com>
2024-06-14 13:38:01 +0530

    Add files via upload

---
+++ Assignment 1/a1_question27.py
@@ -0,0 +1,10 @@
+string1="Pythonisgreat"
+ 
+#printing the string
+print("Actual String: ",string1) 
+   
+#gives us the type of string1
+print("Type of string: ",type(string1))  
+ 
+print("String converted to list :",list(string1)) 
+#prints the list given by list()
